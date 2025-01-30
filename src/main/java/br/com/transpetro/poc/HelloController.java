962ac418b51a791ea72c82dfa8b288e9ac9eeb46
lennon Mello <lennondsantos18@gmail.com>
2025-01-30 08:25:37 -0300

    commmit inicial projeto

---
+++ src/main/java/br/com/transpetro/poc/HelloController.java
@@ -0,0 +1,14 @@
+package br.com.transpetro.poc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Rest Funcionando!";
+	}
+
+}
